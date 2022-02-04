0ca36a0c4e14ca2caecfff03a845c03a568fb3e0
Anastasia Shkalikova <anastasia.shkalikova@gmail.com>
2022-02-04 17:07:56 +0200

    Initial commit

---
+++ Weekday Finder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Weekday Finder
+//
+//  Created by Anastasia on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var dateTF: UITextField!
+  @IBOutlet weak var monthTF: UITextField!
+  @IBOutlet weak var yearTF: UITextField!
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  @IBAction func findDay(_ sender: Any) {
+  }
+  
+}
+

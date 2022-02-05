10023c9e110ee6d8bf011e92a66c090286a4ef59
deboramay <anastasia.shkalikova@gmail.com>
2022-02-05 16:55:43 +0200

    comments added

--- Weekday Finder/ViewController.swift
+++ Weekday Finder/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var resultLabel: UILabel!
   
   @IBAction func findDay(_ sender: Any) {
-    
+    //проверка непустого поля
     guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
     
     let calendar = Calendar.current
@@ -26,17 +26,19 @@ class ViewController: UIViewController {
     
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "ru_Ru")
+    //в каком формате отображать день недели
     dateFormatter.dateFormat = "EEEE"
     
     guard let date = calendar.date(from: dateComponents) else { return }
     
     let weekday = dateFormatter.string(from: date)
+    //с большой буквы
     let capitalizedWeekday = weekday.capitalized
     
     resultLabel.text = capitalizedWeekday
     
   }
-  
+  //пропадает клавиатура при тапании в любом месте экрана
   override func touchesBegan(_ touches: Set<UITouch>, with: UIEvent?) {
     self.view.endEditing(true)
   }
